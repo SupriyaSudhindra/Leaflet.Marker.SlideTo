169de2c632e9216c0fa4282f1d8f2f530ae7ba54
DESKTOP-JCULFC1\Supriya <supriya.sudhindra@politrontech.com>
2018-06-18 17:14:27 +0530

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/Leaflet.Marker.SlideTo.js',
+  output: {
+    filename: 'Leaflet.Marker.SlideTo.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
